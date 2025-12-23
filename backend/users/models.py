bc4f7fa76a60a43ab4bf42ead0db86a52196d840
Angela <anglopoli1@alum.us.es>
2025-12-23 13:51:40 +0100

    feat: Add users with initial models and settings

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+
+    @classmethod
+    def create_user(cls):
+        cls.objects.create(
+            username="testuser",
+            email="testuser@example.com",
+            age=25)
+        
+        cls.objects.create(
+            username="Angela",
+            email="angela@example.com",
+            age=22)
